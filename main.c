12a9dc7767cd762dec7f4244d823ee4f3cc20b45
Phil Zou <zoushuang223@qq.com>
2018-02-01 11:04:11 +0800

    Revert "添加求和函数"
    This reverts commit d416c7991a0c8981bfb00fdb0675560ab28003a4.

--- main.c
+++ main.c
@@ -4,13 +4,7 @@ int MyPrint(void){
 	printf("我的打印函数");
 }
 
-int CalcSum(int a, int b){
-  return a + b;
-}
-
 int main(void){
-  int sum;
   printf("Hello word!");
   MyPrint();
-  sum = CalcSum(4, 3);
 }
